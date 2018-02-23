f0aee9efbde69732c9ee1d57d946c03e3147408a
jwstes <jwstes@gmail.com>
2018-02-23 19:00:18 +0800

    Project Files

---
+++ Server/Server/EncryptedFileTransfer/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace LocalFileTransfer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.disconnectClientToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnstop = new MetroFramework.Controls.MetroButton();
+            this.btnstart = new MetroFramework.Controls.MetroButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoEllipsis = true;
+            this.label1.Font = new System.Drawing.Font("Work Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(542, 51);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Encrypted File Transfer ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(421, 101);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(144, 255);
+            this.listBox1.TabIndex = 2;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.disconnectClientToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(168, 26);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // disconnectClientToolStripMenuItem
+            // 
+            this.disconnectClientToolStripMenuItem.Name = "disconnectClientToolStripMenuItem";
+            this.disconnectClientToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.disconnectClientToolStripMenuItem.Text = "Disconnect Client";
+            this.disconnectClientToolStripMenuItem.Click += new System.EventHandler(this.disconnectClientToolStripMenuItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AllowDrop = true;
+            this.label2.AutoEllipsis = true;
+            this.label2.BackColor = System.Drawing.Color.DimGray;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Work Sans", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(23, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(392, 261);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "+";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.DragDrop += new System.Windows.Forms.DragEventHandler(this.label2_DragDrop);
+            this.label2.DragEnter += new System.Windows.Forms.DragEventHandler(this.label2_DragEnter);
+            this.label2.DragOver += new System.Windows.Forms.DragEventHandler(this.label2_DragOver);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(23, 424);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(542, 26);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(145, 104);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 22);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "1084";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(169, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Listen to port:";
+            // 
+            // label5
+            // 
+            this.label5.AutoEllipsis = true;
+            this.label5.Location = new System.Drawing.Point(23, 394);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(542, 26);
+            this.label5.TabIndex = 14;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnstop
+            // 
+            this.btnstop.Enabled = false;
+            this.btnstop.Location = new System.Drawing.Point(299, 101);
+            this.btnstop.Name = "btnstop";
+            this.btnstop.Size = new System.Drawing.Size(116, 27);
+            this.btnstop.TabIndex = 15;
+            this.btnstop.Text = "Stop Server";
+            this.btnstop.Click += new System.EventHandler(this.btnstop_Click);
+            // 
+            // btnstart
+            // 
+            this.btnstart.Location = new System.Drawing.Point(23, 101);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(116, 27);
+            this.btnstart.TabIndex = 16;
+            this.btnstart.Text = "Start Server";
+            this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(418, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 15);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Clients Connected:";
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(421, 365);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(144, 27);
+            this.metroButton1.TabIndex = 18;
+            this.metroButton1.Text = "Send File";
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 466);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnstart);
+            this.Controls.Add(this.btnstop);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Font = new System.Drawing.Font("Work Sans", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(26, 70, 26, 24);
+            this.Resizable = false;
+            this.Text = "FileTransfer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.ListBox listBox1;
+        public System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem disconnectClientToolStripMenuItem;
+        public System.Windows.Forms.ProgressBar progressBar1;
+        public System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Label label5;
+        private MetroFramework.Controls.MetroButton btnstop;
+        private MetroFramework.Controls.MetroButton btnstart;
+        private System.Windows.Forms.Label label4;
+        private MetroFramework.Controls.MetroButton metroButton1;
+    }
+}
+
---
+++ Server/Server/EncryptedFileTransfer/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Reflection;
+using MetroFramework;
+
+namespace LocalFileTransfer
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        Socket f;
+        Client x;
+        
+        public NotifyIcon notifyicon;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string dir = Environment.CurrentDirectory + "\\FilesReceived\\";
+        void notifyicon_BalloonTipClicked(object sender, EventArgs e)
+        {
+            //open the folder location.
+            System.Diagnostics.Process.Start(dir);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory(dir);
+            FieldInfo iconField = typeof(Form).GetField("defaultIcon", BindingFlags.NonPublic | BindingFlags.Static);
+            Icon myIcon = (Icon)iconField.GetValue(iconField);
+            notifyicon = notifyIcon1;
+            notifyicon.Text = "EncryptedFileTransfer";
+            notifyicon.Visible = true;
+            notifyicon.BalloonTipClicked += notifyicon_BalloonTipClicked;
+            notifyicon.Icon = myIcon;
+            label1.AllowDrop = true;
+            
+            FEvents.connectbutton += FEvents_connectbutton;
+            FEvents.ip += FEvents_ip;
+            FEvents.list += FEvents_list;
+            FEvents.progressbar += FEvents_progressbar;
+            FEvents.status += FEvents_status;
+            FEvents.title += FEvents_title;
+            FEvents.drop += FEvents_drop;
+            Utilities.Tip += Utilities_Tip;
+        }
+
+        void Utilities_Tip(BalloonTipE e)
+        {
+            this.Invoke((MethodInvoker)(() => { notifyIcon1.BalloonTipText = e.Message; notifyIcon1.BalloonTipTitle = e.Title; notifyIcon1.BalloonTipIcon = e.Icon; notifyIcon1.ShowBalloonTip(10000); }));
+        }
+
+        public string LocalIPAddress()
+        {
+            IPHostEntry host;
+            string localIP = "";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                    break;
+                }
+            }
+            return localIP;
+        }
+        
+
+        private void label2_DragEnter(object sender, DragEventArgs e)
+        {
+            if ((e.Data.GetDataPresent(DataFormats.FileDrop) || e.Data.GetDataPresent(DataFormats.Text) || e.Data.GetDataPresent(DataFormats.Html)) && ((x != null && x.Connected) || Utilities.clientx.Count > 0))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+        private void label2_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                new Thread(new ThreadStart(delegate()
+                    {
+                        string[] FileList = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+                        foreach (string i in FileList)
+                        {
+                            if (Directory.Exists(i))
+                            {
+                            }
+                            else
+                            {
+                                if (x != null && x.Connected)
+                                {
+                                    x.Proto.SendFile(i);
+                                }
+                                if (Utilities.clientx.Count > 0)
+                                {
+                                    foreach (Client cli in Utilities.clientx)
+                                    {
+                                        cli.Proto.SendFile(i);
+                                    }
+                                }
+                            }
+                        }
+                    })).Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void label2_DragOver(object sender, DragEventArgs e)
+        {
+            if ((e.Data.GetDataPresent(DataFormats.FileDrop) || e.Data.GetDataPresent(DataFormats.Text) || e.Data.GetDataPresent(DataFormats.Html)) && ((x != null && x.Connected) || Utilities.clientx.Count > 0))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void disconnectClientToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItems.Count > 0)
+            {
+                string ip = listBox1.SelectedItems[0].ToString();
+                foreach (Client cl in Utilities.clientx)
+                {
+                    if (cl.RemoteIp == ip)
+                    {
+                        listBox1.Items.Remove(cl.RemoteIp);
+                        cl.Disconnect();
+                        Utilities.clientx.Remove(cl);
+                        break;
+                    }
+                }
+            }
+        }
+        Thread th;
+
+        void FEvents_drop(ChangedEvent e)
+        {
+            this.Invoke((MethodInvoker)(() => this.label2.Enabled = (bool)e.Message));
+        }
+
+        void FEvents_title(ChangedEvent e)
+        {
+            this.Invoke((MethodInvoker)(() => this.Text = (string)e.Message)); 
+        }
+
+        void FEvents_status(ChangedEvent e)
+        {
+            this.Invoke((MethodInvoker)(() => this.label5.Text = (string)e.Message));
+        }
+
+        void FEvents_progressbar(ChangedEvent e)
+        {
+            this.Invoke((MethodInvoker)(() => this.progressBar1.Value = (int)e.Message));
+        }
+
+        void FEvents_list(ChangedEvent e)
+        {
+            this.Invoke((MethodInvoker)(() => this.listBox1.Items.Remove(e.Message)));
+        }
+
+        void FEvents_ip(ChangedEvent e)
+        {
+        }
+
+        void FEvents_connectbutton(ChangedEvent e)
+        {
+        }
+
+        private void btnstop_Click(object sender, EventArgs e)
+        {
+            f.Close();
+
+            btnstart.Enabled = true;
+            btnstop.Enabled = false;
+            textBox2.ReadOnly = false;
+            label1.Font = new Font("Work Sans", 18);
+            label1.Text = "Encrypted File Transfer ";
+            th.Abort();
+        }
+
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+            f = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            f.Bind(new IPEndPoint(IPAddress.Any, int.Parse(textBox2.Text)));
+            f.Listen(50000);
+            label1.Font = new Font("Work Sans", 10);
+            label1.Text = string.Format("Server started IP : {0}...", LocalIPAddress());
+            btnstart.Enabled = false;
+            btnstop.Enabled = true;
+            textBox2.ReadOnly = true;
+
+            th = new Thread(new ThreadStart(delegate ()
+            {
+                while (true)
+                    try
+                    {
+                        new Thread(new ParameterizedThreadStart(delegate (object _socket)
+                        {
+                            Client cli = new Client((Socket)_socket, true, this, true);
+                            if (MessageBox.Show("Client is connecting from " + cli.RemoteIp + ". Allow this connection?", "Incoming Connection", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == System.Windows.Forms.DialogResult.Yes)
+                            {
+                                cli.Accepted = true;
+
+                                Console.WriteLine("Client connected from " + cli.RemoteIp);
+
+                                Utilities.clientx.Add(cli);
+                                if (listBox1.InvokeRequired)
+                                {
+                                    listBox1.Invoke((MethodInvoker)(() => { listBox1.Items.Add(cli.RemoteIp); }));
+                                }
+                            }
+                            else
+                            {
+                                cli.Disconnect();
+                            }
+                        })).Start(f.Accept());
+                    }
+                    catch { }
+            }));
+            th.Start();
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            if (label5.Visible == true)
+            {
+                label2.Visible = false;
+                label5.Visible = false;
+                progressBar1.Visible = false;
+            }
+            else
+            {
+                label2.Visible = true;
+                label5.Visible = true;
+                progressBar1.Visible = true;
+            }
+
+        }
+    }
+}
